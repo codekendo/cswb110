3c88ae212a0ea420c48ac82628dff995593236fa
codekendo <brandonwong@fastmail.com>
2017-05-04 21:00:11 -0700

    week14 chp13 update

---
+++ about.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+
+  var els = document.getElementsByClassName("placeholder");
+  Array.prototype.forEach.call(els, function(el) {
+
+
+
+    var small = el.querySelector('.img-small');
+
+    // 1: load small image and show it
+    var img = new Image();
+    img.src = small.src;
+    img.onload = function() {
+      small.classList.add('loaded');
+    };
+    // 2: load large image
+    var imgLarge = new Image();
+    //gets the attribute data-large the dataset.large is equal to data-large
+
+    imgLarge.src = el.dataset.large;
+
+
+    imgLarge.onload = function() {
+      imgLarge.classList.add('loaded');
+    };
+    el.appendChild(imgLarge);
+
+  }) //End of ForEach
+}
